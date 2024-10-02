59b48a32ca758b0024d44d7dfcda58e83a18d74f
Sadikur C <sadikurrc@gmail.com>
2024-10-02 16:47:28 +0100

    initial commit

---
+++ backend/src/main/java/com/educache/backend/EducacheBackendApplication.java
@@ -0,0 +1,13 @@
+package com.educache.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducacheBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducacheBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/educache/backend/EducacheBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educache.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducacheBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
