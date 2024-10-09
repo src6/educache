c5538692d1908067cdb5dc259671b6d2f20f8cf7
S Chowdhury <sadikurrc@gmail.com>
2024-10-09 13:59:48 +0100

    Add student entity

---
+++ backend/src/main/java/com/educache/backend/models/Student.java
@@ -0,0 +1,124 @@
+package com.educache.backend.models;
+
+import jakarta.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name = "students")
+
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Date dob;
+
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Parent> parents;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Invoice> invoices;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LessonReport> lessonReports;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Asessment> assessments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Parent> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<Parent> parents) {
+        this.parents = parents;
+    }
+
+    public List<Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(List<Invoice> invoices) {
+        this.invoices = invoices;
+    }
+
+    public List<LessonReport> getLessonReports() {
+        return lessonReports;
+    }
+
+    public void setLessonReports(List<LessonReport> lessonReports) {
+        this.lessonReports = lessonReports;
+    }
+
+    public List<Asessment> getAssessments() {
+        return assessments;
+    }
+
+    public void setAssessments(List<Asessment> assessments) {
+        this.assessments = assessments;
+    }
+}
